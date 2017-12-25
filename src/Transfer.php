7db337ede0569d7b387115cfafe961e8dd133693
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-24 21:51:36 -0500

    Update Transfer.php

--- src/Transfer.php
+++ src/Transfer.php
@@ -1,1 +1,23 @@
 
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Debug> for the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Debug/blob/master/LICENSE> New BSD License.
+ */
+
+namespace Genial\Debug;
+
+/**
+ * Transfer.
+ */
+class Transfer extends Bin\Methods
+{
+    
+}
