7aa9cf4ef31628a578dd5469311b070b11287dde
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-28 00:22:49 -0500

    Update main.bootstrap.php

--- main.bootstrap.php
+++ main.bootstrap.php
@@ -6,15 +6,16 @@
  * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
  *
  * @link      <https://github.com/Genial-Framework/Debug> for the canonical source repository.
- *
  * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
  * @license   <https://github.com/Genial-Framework/Debug/blob/master/LICENSE> New BSD License.
  */
+
 define('DEBUG_TOOLS_ACTIVE', true);
 
-if (!function_exists('env')) {
+if (!defined('ENV_ADAPTER_ACTIVE'))
+{
     trigger_error(
-        'The `env()` function does not exist.',
+        'The `ENV_ADAPTER_ACTIVE` constant does not exist.',
         E_USER_ERROR
     );
 }
