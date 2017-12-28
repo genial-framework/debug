c48784076b1953c7520103ecda5dc6f5e15b4e52
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-28 13:23:31 -0500

    Update Debug.php

--- src/Debug.php
+++ src/Debug.php
@@ -1,1 +1,39 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Debug> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Debug/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Debug;
+
+/**
+ * Debug.
+ */
+class Debug extends Formatter
+{
+    
+    /**
+     * @var array $levels The defualt error reporting levels supported.
+     */
+    private static $levels = [
+        E_ALL,
+        E_DEPRECATED,
+        E_RECOVERABLE_ERROR,
+        E_STRICT,
+        E_COMPILE_WARNING,
+        E_COMPILE_ERROR,
+        E_CORE_WARNING,
+        E_CORE_ERROR,
+        E_NOTICE,
+        E_PARSE,
+        E_WARNING,
+        E_ERROR
+    ];
+  
+}
